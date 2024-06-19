39774cdd1436d0cc4950278158327059cfe48725
Cameron Barnes <cameron_barnes@outlook.com>
2024-06-19 15:46:57 -0700

    finished day 1 part 2

--- day-1/src/part2.rs
+++ day-1/src/part2.rs
@@ -1,17 +1,35 @@
+use std::collections::HashSet;
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-1 - part 2")
+pub fn process(input: &str) -> String {
+    let mut set = HashSet::new();
+    set.insert(0);
+    let mut counter = 0;
+    for offset in input
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .cycle()
+    {
+        counter += offset;
+        if !set.insert(counter) {
+            break;
+        }
+    }
+    counter.to_string()
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
 
-    #[test]
-    fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+    #[rstest]
+    #[case("+1\n-1", "0")]
+    #[case("+3\n+3\n+4\n-2\n-4", "10")]
+    #[case("-6\n+3\n+8\n+5\n-6", "5")]
+    #[case("+7\n+7\n-2\n-7\n-4", "14")]
+    fn test_process(#[case] input: &str, #[case] expected: &str) {
+        assert_eq!(expected, process(input));
     }
 }
