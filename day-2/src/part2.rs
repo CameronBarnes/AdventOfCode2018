4b2330e06eb8fb29bcadc4f5269eac56e45f7f34
Cameron Barnes <cameron_barnes@outlook.com>
2024-06-20 15:16:30 -0700

    finished day 2 part 2

--- day-2/src/part2.rs
+++ day-2/src/part2.rs
@@ -1,7 +1,32 @@
+use itertools::Itertools;
+
+fn compare(first: &str, second: &str) -> Option<String> {
+    let vec = first
+        .chars()
+        .zip(second.chars()).enumerate()
+        .filter_map(|(index, (a, b))| a.ne(&b).then_some(index))
+        .collect_vec();
+    if vec.len() == 1 {
+        let mut first = first.to_string();
+        first.remove(vec[0]);
+        Some(first)
+    } else {
+        None
+    }
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-2 - part 2")
+pub fn process(input: &str) -> String {
+    let lines = input.lines().collect_vec();
+
+    for (i, a) in lines.iter().enumerate() {
+        for b in lines.iter().skip(i + 1) {
+            if let Some(out) = compare(a, b) {
+                return out;
+            }
+        }
+    }
+    panic!("None found")
 }
 
 #[cfg(test)]
@@ -10,8 +35,13 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "abcde
+fghij
+klmno
+pqrst
+fguij
+axcye
+wvxyz";
+        assert_eq!("fgij", process(input));
     }
 }
