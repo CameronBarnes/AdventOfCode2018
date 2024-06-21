f825aef72f4a48dda7662e0b75a9b4e3a978642c
Cameron Barnes <cameron_barnes@outlook.com>
2024-06-20 17:13:16 -0700

    finished day 3 part 2

--- day-3/src/part2.rs
+++ day-3/src/part2.rs
@@ -1,7 +1,63 @@
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Rect {
+    left: usize,
+    right: usize,
+    top: usize,
+    bottom: usize,
+}
+
+impl Rect {
+    const fn new(x: usize, y: usize, width: usize, height: usize) -> Self {
+        Self {
+            left: x,
+            right: x + width,
+            top: y,
+            bottom: y + height,
+        }
+    }
+
+    fn overlaps(&self, other: &Self) -> bool {
+        let left = self.left.max(other.left);
+        let right = self.right.min(other.right);
+        let top = self.top.max(other.top);
+        let bottom = self.bottom.min(other.bottom);
+        left < right && top < bottom
+    }
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-3 - part 2")
+pub fn process(input: &str) -> String {
+    let rects = input
+        .lines()
+        .map(|line| line.split_once(" @ ").unwrap())
+        .map(|(id, line)| (id.strip_prefix('#').unwrap(), line.split_once(": ").unwrap()))
+        .map(|(id, (coords, size))| {
+            let (x, y) = coords.split_once(',').unwrap();
+            let (x, y) = (x.parse().unwrap(), y.parse().unwrap());
+            let (width, height) = size.split_once('x').unwrap();
+            let (width, height) = (width.parse().unwrap(), height.parse().unwrap());
+            (id, Rect::new(x, y, width, height))
+        })
+        .collect_vec();
+
+    for (id, a) in &rects {
+        let mut collision = false;
+        for (id_b, b) in &rects {
+            if id.eq(id_b) {
+                continue;
+            }
+            if a.overlaps(b) {
+                collision = true;
+                break;
+            }
+        }
+        if !collision {
+            return (*id).to_string();
+        }
+    }
+    panic!("None found");
 }
 
 #[cfg(test)]
@@ -10,8 +66,9 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "#1 @ 1,3: 4x4
+#2 @ 3,1: 4x4
+#3 @ 5,5: 2x2";
+        assert_eq!("3", process(input));
     }
 }
