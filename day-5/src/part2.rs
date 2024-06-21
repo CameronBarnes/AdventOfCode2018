2dfbb002fcf0edd12df104ddee34303f3e42fe41
Cameron Barnes <cameron_barnes@outlook.com>
2024-06-21 12:28:51 -0700

    finished day 5 part 2

--- day-5/src/part2.rs
+++ day-5/src/part2.rs
@@ -1,7 +1,31 @@
+const fn swap_case(c: char) -> char {
+    if c.is_ascii_uppercase() {
+        c.to_ascii_lowercase()
+    } else {
+        c.to_ascii_uppercase()
+    }
+}
+
+fn react(input: &str) -> usize {
+    let mut out = Vec::with_capacity(input.len());
+    input.trim_end().chars().for_each(|c| {
+        if !out.is_empty() && *out.last().unwrap() == swap_case(c) {
+            out.pop().unwrap();
+        } else {
+            out.push(c);
+        }
+    });
+    out.len()
+}
 
 #[tracing::instrument]
-pub fn process(_input: &str) -> String {
-    todo!("day-5 - part 2")
+pub fn process(input: &str) -> String {
+    ('a'..='z')
+        .map(|c| input.replace([c, c.to_ascii_uppercase()], ""))
+        .map(|line| react(&line))
+        .min()
+        .unwrap()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -10,8 +34,7 @@ mod tests {
 
     #[test]
     fn test_process() {
-        todo!("Havent built test yet");
-        let input = "";
-        assert_eq!("", process(input));
+        let input = "dabAcCaCBAcCcaDA";
+        assert_eq!("4", process(input));
     }
 }
